d472a23e724d054e153f9a5bbb414353dc675dca
Blessing2023-cell <128575583+Blessing2023-cell@users.noreply.github.com>
2023-03-27 12:44:43 +0200

    Add files via upload

---
+++ Blessing Banda Portfolio/script.js
@@ -0,0 +1,19 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Graphics Designer","UX UI Designer", "Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    Ioop: true
+
+
+})
+
+var typed = new Typed(".multiple-text1",{
+    strings:["Graphics Designer","Web Designer", "UX UI Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    Ioop: true
+
+
+})
